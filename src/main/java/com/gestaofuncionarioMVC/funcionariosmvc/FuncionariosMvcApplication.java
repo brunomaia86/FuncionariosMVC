324431aa52510efa8d4da70b4bac34bf58be7216
Bruno Maia <brunomaia86@gmail.com>
2018-05-10 15:45:17 -0300

    commit inicial

---
+++ src/main/java/com/gestaofuncionarioMVC/funcionariosmvc/FuncionariosMvcApplication.java
@@ -0,0 +1,12 @@
+package com.gestaofuncionarioMVC.funcionariosmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuncionariosMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuncionariosMvcApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gestaofuncionarioMVC/funcionariosmvc/FuncionariosMvcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gestaofuncionarioMVC.funcionariosmvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FuncionariosMvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
