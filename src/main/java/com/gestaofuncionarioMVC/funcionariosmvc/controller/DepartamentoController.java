e15995407957aeeb0b60f53cec9d65ee2892e169
Bruno Maia <brunomaia86@gmail.com>
2018-05-15 16:50:05 -0300

    cadastrar departamento

--- src/main/java/com/gestaofuncionarioMVC/funcionariosmvc/controller/DepartamentoController.java
+++ src/main/java/com/gestaofuncionarioMVC/funcionariosmvc/controller/DepartamentoController.java
@@ -1,20 +1,34 @@
 package com.gestaofuncionarioMVC.funcionariosmvc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.gestaofuncionarioMVC.funcionariosmvc.domain.Departamento;
+import com.gestaofuncionarioMVC.funcionariosmvc.repository.DepartamentoRepository;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
+	
+	@Autowired
+	DepartamentoRepository departamentoRepository;
 
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Departamento departamento) {
 		return "/departamento/cadastro";
 	}
 
 	@GetMapping("/listar")
 	public String listar() {
+		return "/departamento/lista";
+	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Departamento departamento) {
+		departamentoRepository.save(departamento);
 		return "/departamento/lista";
 	}
 }
